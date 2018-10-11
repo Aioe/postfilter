d7af7082d03891d2fe32e7b82b6a42b4b363cc9f
root <root@secondario.invalid>
2018-10-11 15:58:05 +0200

    custom.pm is lighter

--- src/modules/style.pm
+++ src/modules/style.pm
@@ -778,6 +778,69 @@ if ( $hdr{'Sender'} ne "" )
 	&delete_headers("X-No-Archive") if ( $config{'delete_header_x-no-archive'} eq "true" );
 
 #######################
+# Invalid chars
+#######################
+
+
+	my @headers_to_check = ( "From", "Newsgroups", "Followup-To", "Subject" );
+
+
+	foreach( @headers_to_check )
+	{
+		my $result = &check_unknownchars($_);
+		return $result if ($result != 0);
+	}
+
+######################
+# Distribution
+######################
+
+if ($hdr{'References'} ne "")
+	{
+		my $refers = $hdr{'References'};
+		my @references = split(/\</, $refers);
+		my $lastref = "<" . $references[-1];	# estrae l'ultima referenza
+
+		my $fate = &check_localdistribution($lastref);
+		
+		if ($fate == 1)  # se il messaggio è una risposta ad un messaggio che contiene Distribution: local
+		{
+			if ($hdr{'Distribution'} !~ /usenet|local/i)	# e se non contiene "local" od "usenet" nel suo header Distribution
+			{
+				&log("notice", "This message is a reply to a local message ($lastref)");
+				&log("notice", "Local message");
+				$hdr{'Distribution'} = "local";
+				my $exit = &save_localdistribution($hdr{'Message-ID'});
+				return $exit if ($exit != 0);
+			}		
+		}
+	
+	}
+	
+        if ($hdr{'Distribution'} ne "" )
+        {
+                if ($hdr{'Distribution'} =~ /^local$/i )
+                {
+                        &log("notice", "Message with LOCAL Distribution");
+                        &log("notice", "Local message");
+                        my $fate = &save_localdistribution($hdr{'Message-ID'});
+                        return $fate if ($fate != 0);
+                }
+                elsif ($hdr{'Distribution'} =~ /^usenet$/i )
+                {
+                        &log("notice", "Message includes USENET Distribution");
+                        &delete_headers("Distribution")
+                }
+                else
+                {
+                        my $dist = $hdr{'Distribution'};
+                        $quickref[108] = "Invalid Distribution ($dist)";
+                        return 108;
+                }
+	}
+
+
+#######################
 # New Headers
 #######################
 
