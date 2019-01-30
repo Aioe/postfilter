b62f4da49ab067ab265139cae5cd92bb51ba39ea
root <root@secondario.invalid>
2019-01-30 14:23:27 +0100

    Bugfix

--- src/postfilter.pl
+++ src/postfilter.pl
@@ -29,8 +29,8 @@ my $use_innconfval	= "true";				# whether to use innconfval in order to determin
 								# are read from postingaccess.conf (that in this case is needed to
 								# be properly configured).
 
-my $innconfval 		= "/usr/lib/news/bin//innconfval";				# not needed if $use_innshellvar eq "false"
-my $config_dir 		= "/etc/news/postfilter/"; 			# used only if $use_innshellvar eq "false"
+my $innconfval 		= "INNCONFVAL";				# not needed if $use_innshellvar eq "false"
+my $config_dir 		= "POSTETCDIR"; 			# used only if $use_innshellvar eq "false"
 
 my @files	= (						# configuration files that need to be loaded before analyzing each post
 			"postfilter.conf",
